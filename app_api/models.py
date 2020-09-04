b24bbdce55757f8495b727a0f4ad50282f99993c
craig-mini <cm.nicol@outlook.com>
2020-09-04 09:06:21 +0100

    define model properly

--- app_api/models.py
+++ app_api/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 
 class TodoItem(models.Model):
+
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=128)
     status = models.BooleanField(default=False)
+
+    def __init__(self, title, description):
+        self.title = title
+        self.description = description
 
     def __str__(self):
         return self.title
--- app_api/serializers.py
+++ app_api/serializers.py
@@ -1,8 +1,32 @@
 from .models import TodoItem
 from rest_framework import serializers
 
+"""
+From Django REST framework API Guide
 
-class TodoItemSerializer(serializers.HyperlinkedModelSerializer):
+https://www.django-rest-framework.org/api-guide/serializers/#serializers
+"Serializers allow complex data such as querysets and model instances to be converted to native Python datatypes that 
+can then be easily rendered into JSON, XML or other content types. Serializers also provide deserialization, 
+allowing parsed data to be converted back into complex types, after first validating the incoming data." 
+"""
+
+
+class TodoItemSerializer(serializers.ModelSerializer):
+
+    """
+    https://www.django-rest-framework.org/api-guide/serializers/#modelserializer
+    The ModelSerializer class is the same as a regular Serializer class, except that:
+        - It will automatically generate a set of fields for you, based on the model.
+        - It will automatically generate validators for the serializer, such as unique_together validators.
+        - It includes simple default implementations of .create() and .update().
+    """
+
     class Meta:
         model = TodoItem
-        fields = ['id', 'title', 'description']
+        fields = ['id', 'title', 'description', 'status']
+
+
+
+# from app_api.serializers import TodoItemSerializer
+# serializer = TodoItemSerializer()
+# print(repr(serializer))
